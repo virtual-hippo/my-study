40ddd7b33e78dd800cfa2c3c11b68c01a7518951
virtual-hippo <60084645+virtual-hippo@users.noreply.github.com>
2022-03-30 20:28:35 +0900

    update: added the features to specify the question number in the command line argument

--- rust/notation-study/kyopro_educational_90/src/main.rs
+++ rust/notation-study/kyopro_educational_90/src/main.rs
@@ -1,5 +1,13 @@
+use std::env;
 mod sol_001;
 
 fn main() {
-    sol_001::call_solve();
+    let args: Vec<String> = env::args().collect();
+    let problem_id = args[1].parse::<i32>().unwrap();
+    match problem_id {
+        1 => sol_001::call_solve(),
+        //2 => sol_002::call_solve(),
+        //3 => sol_003::call_solve(),
+        _ => println!("The sol {} couldn't find.", problem_id)
+    }
 }
